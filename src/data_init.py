7457fa72ecd88aa2a749a0c0330dccfd8caaad19
mk17ru <mk17.ru@mail.ru>
2024-04-04 00:00:29 +0300

    add link

--- src/data_init.py
+++ src/data_init.py
@@ -3,6 +3,7 @@ import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
 API = os.environ.get("API")
+WEBUI_BASE_URL = os.environ.get("WEBUI_BASE_URL")
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
