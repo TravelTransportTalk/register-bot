770a6124a4bf1565661fb0b241fd4c730adf726f
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2024-04-05 02:26:33 +0300

    feat: use webhooks instead of long polling

--- src/bot_init.py
+++ src/bot_init.py
@@ -9,7 +9,10 @@ from telegram.ext import (
 
 from src.behavior.default_handler import default_builder
 from src.behavior.update_handler import update_builder
-from src.data_init import TOKEN
+from src.data_init import (
+    TOKEN, WEBHOOK_PUBLIC_URL, WEBHOOK_BIND_HOST,
+    WEBHOOK_BIND_PORT, WEBHOOK_SECRET
+)
 
 
 import asyncio
@@ -34,7 +37,14 @@ def bot_start() -> None:
     )
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    # application.run_polling(allowed_updates=Update.ALL_TYPES)
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=int(WEBHOOK_BIND_PORT),
+        url_path=WEBHOOK_PUBLIC_URL,
+        max_connections=100,
+        secret_token=WEBHOOK_SECRET
+    )
 
 
 def __init_commands(application, commands: List[BotCommand]):
--- src/data_init.py
+++ src/data_init.py
@@ -4,6 +4,12 @@ import os
 TOKEN = os.environ.get("BOT_TOKEN")
 API = os.environ.get("API")
 WEBUI_BASE_URL = os.environ.get("WEBUI_BASE_URL")
+WEBHOOK_PUBLIC_URL = os.environ.get("WEBHOOK_PUBLIC_URL")
+WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET")
+WEBHOOK_BIND_HOST = os.environ.get("WEBHOOK_BIND_HOST", "0.0.0.0")
+WEBHOOK_BIND_PORT = os.environ.get("WEBHOOK_BIND_PORT", "8080")
+
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
